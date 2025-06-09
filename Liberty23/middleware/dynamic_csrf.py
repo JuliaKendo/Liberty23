4a598b997d7d284abfeef2c4ab8d633a85b4af17
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2025-06-09 13:53:22 +0300

    update dynamic csrf middleware

--- Liberty23/middleware/dynamic_csrf.py
+++ Liberty23/middleware/dynamic_csrf.py
@@ -1,3 +1,4 @@
+from django.middleware.csrf import CsrfViewMiddleware
 from django.conf import settings
 
 class DynamicCsrfTrustedOriginsMiddleware:
@@ -16,3 +17,15 @@ class DynamicCsrfTrustedOriginsMiddleware:
         response = self.get_response(request)
 
         return response
+    
+
+class DynamicCsrfViewMiddleware(CsrfViewMiddleware):    
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        host = request.headers.get('X-Forwarded-Host') or request.get_host()
+        scheme = request.scheme
+        trusted_origin = f"{scheme}://{host}{f':{settings.ALLOWED_PORT}' if settings.ALLOWED_PORT else '' }"
+
+        if trusted_origin not in settings.CSRF_TRUSTED_ORIGINS:
+            settings.CSRF_TRUSTED_ORIGINS.append(trusted_origin)
+
+        return super().process_view(request, callback, callback_args, callback_kwargs)
